18ddc866c486818e912eb45194533f1ef74ac38b
e <msamisami1112@gmail.com>
2024-09-09 01:15:39 +0500

    apply searching products with filter function and update the products array with filtered array

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "../css/product.css";
+
 const ProductPage = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]); // All products
+  const [searchedProducts, setSearchedProducts] = useState([]); // Filtered products
+
   const fetchProducts = async () => {
-    const response = await axios.get("https://fakestoreapi.com/products");
-    console.log(response.data);
-    setProducts(response.data);
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data); // Update state with the fetched products
+      setSearchedProducts(response.data); // Initialize searchedProducts with all products
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   useEffect(() => {
@@ -16,15 +23,28 @@ const ProductPage = () => {
   const handleDelete = (id) => {
     // Create a new array excluding the product with the given id
     const updatedProducts = products.filter((product) => product.id !== id);
-    // Update the state with the new array
+    // Update both states with the new array
     setProducts(updatedProducts);
+    setSearchedProducts(updatedProducts);
   };
+
+  const searchHandler = (e) => {
+    const userInput = e.target.value;
+    const searchResult = products.filter((item) => {
+      if (item.title.toLowerCase().includes(userInput.toLowerCase())) {
+        return true;
+      }
+    });
+
+    setSearchedProducts(searchResult);
+  };
+
   return (
     <>
       <h1>This Is Products Page</h1>
-
+      <input onChange={searchHandler} placeholder="Search through products" />
       <div className="products-container">
-        {products.map((product) => (
+        {searchedProducts.map((product) => (
           <div key={product.id} className="product-card">
             <img src={product.image} alt={product.title} />
             <h2>Price: ${product.price}</h2>
